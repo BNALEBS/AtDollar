568dd6c85bb5f6c882181b8f2698369b51fe7b84
Zhengwei Chu <cloverchu@hotmail.com>
2017-06-06 18:42:02 -0700

    modify_time 6/6 02

--- src/mruset.h
+++ src/mruset.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2012 The Atdollar developers
+// Copyright (c) 2015-2017 The Atdollar Developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
